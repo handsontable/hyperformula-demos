4f1f4122e8a5a15a9db98dd316a31acd274fffc3
Jan Siegel <js.ziggle@gmail.com>
2020-06-16 15:32:26 +0200

    - Move index.js to /src - Retrieve the original package.json from codesandbox - Remove the milligram dependency in favor of cdn

---
+++ basic-usage/src/index.js
@@ -0,0 +1,10 @@
+import { renderTable } from "./renderers";
+import { bindEvents } from "./ui";
+
+import "./styles.css";
+
+// Bind the button events.
+bindEvents();
+
+// Render the table.
+renderTable();
