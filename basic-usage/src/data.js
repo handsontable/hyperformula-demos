82a30fb02759da9ad58723d03567f3828981a1fd
Ion Alberdi <ion.alberdi@pricemoov.com>
2021-11-24 11:49:46 +0100

    [basic-usage] Some benchmark

--- basic-usage/src/data.js
+++ basic-usage/src/data.js
@@ -1,4 +1,11 @@
 /**
  * Initial table data.
  */
-export const tableData = [["10", "20", "=SUM(A1,B1)"]];
+var buildingTableData = [["headerA", "headerB", "10"]];
+export const nbRows = 1000; // <5K: 5ms: 10K: 32ms
+for (let i=1; i<=nbRows; i++) {
+    buildingTableData.push([i.toString(), (i + 10).toString(),
+                            `=SUM(A${i+1}, B${i+1}) + C1`]);
+}
+
+export const tableData = buildingTableData;
--- basic-usage/src/ui.js
+++ basic-usage/src/ui.js
@@ -1,4 +1,5 @@
 import { hf, sheetId } from "./hyperformulaConfig";
+import { nbRows } from "./data";
 
 /**
  * Bind the events to the buttons.
@@ -7,11 +8,18 @@ export function bindEvents() {
   const calculateButton = document.querySelector("#calculate");
   const formulaPreview = document.querySelector("#address-preview");
   const calculationResult = document.querySelector("#result");
-  const cellAddress = { sheet: sheetId, row: 0, col: 2 };
-
-  formulaPreview.innerText = hf.simpleCellAddressToString(cellAddress, sheetId);
+  const cellAddress = { sheet: sheetId, row: 2, col: 2 };
+  const cellName = hf.simpleCellAddressToString(cellAddress, sheetId);
+  formulaPreview.innerText = cellName;
 
   calculateButton.addEventListener("click", () => {
-    calculationResult.innerText = hf.getCellValue(cellAddress);
+        const before = window.performance.now();
+        for (let i=1; i<=nbRows; i++) {
+          const oneCellAddress = { sheet: sheetId, row: i, col: 2 };
+          const res = hf.getCellValue(oneCellAddress);
+        }
+        const dur = window.performance.now() - before;
+        const res = hf.getCellValue(cellAddress);
+        calculationResult.innerText = `${res} (${dur} ms)`;
   });
 }
