d81e53e56c15be2f88608f6ae1c3ea740bd7914d
Marcin Warpechowski <warpech@gmail.com>
2022-12-16 11:27:28 +0100

    add some data and evaluate it

--- custom-functions-ts/index.ts
+++ custom-functions-ts/index.ts
@@ -17,3 +17,26 @@ const hf = HyperFormula.buildEmpty({
   localeLang: 'en-US',
   licenseKey: "gpl-v3",
 });
+
+const sheetName = hf.addSheet("main");
+const sheetId = hf.getSheetId(sheetName);
+
+const tableData = [
+  ["Greg Black", "11:45 AM", "05/23/1989", "=YEAR(NOW())-YEAR(C1)", "$80,000.00"],
+  ["Anne Carpenter", "12:30 PM", "01/01/1980", "=YEAR(NOW())-YEAR(C2)", "$95,000.00"],
+  ["Natalie Dem", "1:30 PM", "12/13/1973", "=YEAR(NOW())-YEAR(C3)","$78,500.00"],
+  ["John Sieg", "2:00 PM", "10/31/1995", "=YEAR(NOW())-YEAR(C4)", "$114,000.00"],
+  ["Chris Aklips", "11:30 AM", "08/18/1987", "=YEAR(NOW())-YEAR(C5)", "$71,900.00"],
+  ["AVERAGE", null, null, "=AVERAGE(D1:D5)", "=AVERAGE(E1:E5)"]
+];
+
+hf.setCellContents(
+  {
+    row: 0,
+    col: 0,
+    sheet: sheetId
+  },
+  tableData
+);
+
+console.log(hf.getAllSheetsValues());
