472258327c1ab4d0666e7331d5e569c190f0e9b8
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-09-21 11:46:05 +0200

    Smaller heading

--- performance/src/data.js
+++ performance/src/data.js
@@ -1,4 +1,4 @@
 /**
  * Initial table data.
  */
- export const tableData = Array(Math.ceil(1000000/3)).fill(0).map((_, i) => Array(3).fill(0).map((_, j) => `Data$${i}${j}`));
+ export const tableData = Array(Math.ceil(1000000/5)).fill(0).map((_, i) => Array(5).fill(0).map((_, j) => `Data$${i}${j}`));
