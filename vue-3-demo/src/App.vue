596ec00697beebbda7ae901d68c3cecfb3214d21
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-09-27 13:15:42 +0200

    Display 2 buttons

--- vue-3-demo/src/App.vue
+++ vue-3-demo/src/App.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import 'milligram'
+
+import ActionButton from './components/ActionButton.vue'
+
+function runCalculations() {
+  console.log('run')
+}
+
+function reset() {
+  console.log('reset')
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div>
+      <ActionButton text="Run calculations" @button-click="runCalculations" />
+      <ActionButton text="Reset" outline @button-click="reset" />
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  body {
+    font-family: sans-serif;
+    counter-reset: row-counter;
+    padding: 15px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
