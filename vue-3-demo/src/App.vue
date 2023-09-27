ff9065be5765849536d7b1c0bde53b131c297f8b
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-09-27 14:54:08 +0200

    Display a table

--- vue-3-demo/src/App.vue
+++ vue-3-demo/src/App.vue
@@ -2,6 +2,10 @@
 import 'milligram'
 
 import ActionButton from './components/ActionButton.vue'
+import HfTable from './components/HfTable.vue'
+
+const data = [['Lorem Ipsum', '2', '3', '4', '5']]
+const totals = ['total 1', 'total 2']
 
 function runCalculations() {
   console.log('run')
@@ -17,14 +21,15 @@ function reset() {
     <div>
       <ActionButton text="Run calculations" @button-click="runCalculations" />
       <ActionButton text="Reset" outline @button-click="reset" />
+      <HfTable :data="data" :totals="totals" />
     </div>
   </main>
 </template>
 
 <style scoped>
-  body {
-    font-family: sans-serif;
-    counter-reset: row-counter;
-    padding: 15px;
-  }
+body {
+  font-family: sans-serif;
+  counter-reset: row-counter;
+  padding: 15px;
+}
 </style>
