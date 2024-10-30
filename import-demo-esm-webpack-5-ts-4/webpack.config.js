b66e50603c95a701c5984a26f136409573ac04c0
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2024-10-30 15:06:15 +0100

    Add webpack+TS demos

--- import-demo-esm-webpack-5-ts-4/webpack.config.js
+++ import-demo-esm-webpack-5-ts-4/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
   entry: './src/index.ts',
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
@@ -19,15 +18,5 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    historyApiFallback: true,
-    compress: true,
-    hot: true,
-    port: 8080,
   },
 };
--- import-demo-esm-webpack-5-ts-5/webpack.config.js
+++ import-demo-esm-webpack-5-ts-5/webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
