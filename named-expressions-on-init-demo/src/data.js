0514a20f2fe3ccc447d6482cf8de9aa7b2f13f96
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2025-08-13 13:12:40 +0200

    Update layout of the named-expressions-on-init demo

--- named-expressions-on-init-demo/src/data.js
+++ named-expressions-on-init-demo/src/data.js
@@ -2,8 +2,8 @@
  * Initial table data.
  */
  export const tableData = [
-  ["Annual Interest Rate", .08, ''],
-  ["Number Of Months", 360, ''],
-  ["Loan Amount", 80000, ''],
-  ['Monthly Payment', '=PMT(AnnualInterestRate/12, NumberOfMonths, -LoanAmount)', ''],
+  ["Annual Interest Rate", .08],
+  ["Number Of Months", 360],
+  ["Loan Amount", 80000],
+  ['Monthly Payment', '=PMT(AnnualInterestRate/12, NumberOfMonths, -LoanAmount)'],
 ];
